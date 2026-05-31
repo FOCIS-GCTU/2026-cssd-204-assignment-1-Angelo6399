848763d2709937ee0de5871b38cde4828d9228f3
Angelo6399 <2425402742@live.gctu.edu.gh>
2026-05-31 23:52:57 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Agboka Angelo Sena
+# STUDENT ID: 2425402742
+# Email: 2425402742@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Agboka Angelo Sena, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
+    print()
+    print('...AAS')
+    print()
+    print('......AAAAAA..............AAAAAA.............SSSSSSSSSS.....')
+    print('.....AAAAAAAA............AAAAAAAA...........SSSSSSSSSSS.....')
+    print('....AAA....AAA..........AAA....AAA.........SSSS.............')
+    print('....AAA....AAA..........AAA....AAA.........SSSS.............')
+    print('...AAAA....AAAA........AAAA....AAAA..........SSSSSS.........')
+    print('...AAAAAAAAAAAA........AAAAAAAAAAAA............SSSSSS.......')
+    print('...AAAAAAAAAAAA........AAAAAAAAAAAA................SSSS.....')
+    print('...AAAA....AAAA........AAAA....AAAA................SSSS.....')
+    print('...AAAA....AAAA...**...AAAA....AAAA...**...SSSSSSSSSSS....**')
+    print('...AAAA....AAAA...**...AAAA....AAAA...**...SSSSSSSSSS.....**')
+    print()
 
 
 main()
